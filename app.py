92fb74aa2b8754f3245d3d9f41c10c6b03ad7a54
aaarell <143507800+aaarell@users.noreply.github.com>
2023-12-28 23:51:04 -0600

    Update

--- app.py
+++ app.py
@@ -1,59 +1,64 @@
-from flask import Flask, render_template, request
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-import numpy as np
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Gets rid of GPU errors from tensorflow
+
 import time
+import os
+import cv2
+import numpy as np
+from PIL import Image
+from flask import Flask, request, redirect, render_template
+from tensorflow.keras.models import load_model
+from tensorflow.keras.models import model_from_json
 
-app = Flask(__name__, static_url_path='/static')
-app.config['UPLOAD_FOLDER'] = 'uploads'
-model = load_model("mobilenet_model.h5")
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static'
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 
-def preprocess_image(image_path):
-    img = load_img(image_path, target_size=(224, 224))
-    img_array = img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    return img_array
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
-def predict_image(image_path):
-    start_time = time.time()
 
-    img_array = preprocess_image(image_path)
-    result = model.predict(img_array)
-
-    end_time = time.time()
-    prediction_time = end_time - start_time
-
-    label = np.argmax(result)
-    classes = ['Paper', 'Rock', 'Scissors']
-    prediction = classes[label]
-    accuracy = result[0][label]
-
-    return prediction, accuracy, prediction_time
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/")
+def index():
+    return render_template('/select.html', )
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        file = request.files['image']
-        image_filename = file.filename
-        image_path = os.path.join('static', image_filename)
+    chosen_model = request.form['select_model']
+    model_dict = {'MobileNet Model'   :   'static/MobileNetrps.h5',
+                  'ResNet Model'     :   'static/ResNetrps.h5',}
+    if chosen_model in model_dict:
+        model = load_model(model_dict[chosen_model])
+    else:
+        model = load_model(model_dict[0])
+    file = request.files["file"]
+    image_filename = file.filename
+    file.save(os.path.join('static', image_filename))
+    img = cv2.cvtColor(np.array(Image.open(file)), cv2.COLOR_BGR2RGB)
+    img = np.expand_dims(cv2.resize(img, model.layers[0].input_shape[0][1:3] \
+    if not model.layers[0].input_shape[1:3] else model.layers[0].input_shape \
+    [1:3]).astype('float32') / 255, axis=0)
+    start = time.time()
+    pred = model.predict(img)[0]
+    labels = (pred > 0.5).astype(int)
+    print(labels)
+    runtimes = round(time.time()-start,4)
+    respon_model = [round(elem * 100, 2) for elem in pred]
+    return predict_result(chosen_model, runtimes, respon_model, image_filename)
 
-        file.save(image_path)
-        prediction, accuracy, prediction_time = predict_image(image_path)
-
-        # Get current timestamp
-        current_time = int(time.time())
-
-        # Dictionary untuk memetakan label prediksi ke label gambar yang sesuai
-        label_mapping = {'Paper': 'paper', 'Rock': 'rock', 'Scissors': 'scissors'}
-        prediction_label = label_mapping.get(prediction, 'Unknown')
-
-        return render_template('index.html', prediction=prediction_label, accuracy=accuracy,
-                               prediction_time=prediction_time, image_filename=image_filename, current_time=current_time)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def predict_result(model, run_time, probs, image_filename):
+    class_list = {'Paper': 0, 'Rock': 1, 'Scissors': 2}
+    idx_pred = probs.index(max(probs))
+    labels = list(class_list.keys())
+    return render_template('/result_select.html', labels=labels,
+                            probs=probs, model=model, pred=idx_pred,
+                            run_time=run_time, image_filename=image_filename)
+    
+if __name__ == "__main__":
+        app.run(debug=True, host='0.0.0.0', port=2000)
